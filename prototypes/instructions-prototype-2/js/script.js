63a74c0b7b734cd071309e4541f2aff771b72c25
sydneylttan-dot <sydney.lt.tan@gmail.com>
2026-09-21 20:36:09 -0400

    Finished prototype 2
    - project that changes look depending on mouse position
    - added all the elements in the project
    - need to add describe

--- prototypes/instructions-prototype-2/js/script.js
+++ prototypes/instructions-prototype-2/js/script.js
@@ -5,13 +5,75 @@
 
 "use strict";
 
+let circleSize
+let bgSize
+
 function setup() {
     createCanvas(800, 800);
+
+    circleSize = 100;
+    bgSize = 1000;
 }
 
 
 function draw() {
-    background(0);
+    background(20, 100, 100);
+
+    //size variable changes
+    circleSize = circleSize + random(-10, 10);
+    bgSize = bgSize + random(-10, 10);
+
+    //black circle in the bg
+    fill(0)
+    noStroke();
+    circle(width / 2, height / 2, bgSize);
+
+    //white ellipse
+    push();
     fill(255);
-    circle(width / 2, height / 2, 400);
+    noStroke();
+    ellipse(width / 2, height / 2, mouseY, mouseX);
+    pop();
+
+    //cyan ellipse
+    push();
+    fill(100, 190, 220, 180);
+    noStroke();
+    ellipse(width / 2, height / 2, mouseX - 20, mouseY - 20);
+    pop();
+
+    //magenta square
+    push();
+    fill(170, 40, 190, 180);
+    noStroke();
+    rectMode(CENTER);
+    square(width / 2, height / 2, mouseX - 100, 60);
+    pop();
+
+    //green circle
+    push();
+    fill(80, 200, 150, 150);
+    noStroke();
+    circle(width / 2, height / 2, mouseX - 600);
+    pop();
+
+    //green square
+    push();
+    fill(40, 225, 100, 140);
+    noStroke();
+    rectMode(CENTER);
+    square(width / 2, height / 2, mouseY - 100, 70);
+    pop();
+
+    push();
+    fill(255, 255, 255, 50);
+    noStroke();
+    circle(width / 2, height / 2, circleSize);
+    pop();
+
+    fill(255, 255, 255, 50);
+    noStroke();
+    circle(random(0, width), random(0, height), random(1, 5));
+    circle(random(0, width), random(0, height), random(1, 5));
+
 }
